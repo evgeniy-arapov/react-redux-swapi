188051a47f4d436d77c7c2ebcb01d2984515f8a6
Евгений Арапов <arapov.evgeniy@gmail.com>
2019-10-20 15:26:48 +0300

    Updates

---
+++ precache-manifest.322ed8c9ccf32b9e353bcd6c258cb29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9cb3a7f16da75a994f3f5d10c63be69",
+    "url": "/react-redux-swapi/index.html"
+  },
+  {
+    "revision": "7a0aafc8094a2216a4a8",
+    "url": "/react-redux-swapi/static/css/main.06180619.chunk.css"
+  },
+  {
+    "revision": "3785435c603af368557c",
+    "url": "/react-redux-swapi/static/js/2.682b8f4b.chunk.js"
+  },
+  {
+    "revision": "7a0aafc8094a2216a4a8",
+    "url": "/react-redux-swapi/static/js/main.59d74f37.chunk.js"
+  },
+  {
+    "revision": "062fff63e9434928aac4",
+    "url": "/react-redux-swapi/static/js/runtime~main.45599c69.js"
+  },
+  {
+    "revision": "dccef97f1dabc65dc97e875a891bf95f",
+    "url": "/react-redux-swapi/static/media/logo.dccef97f.jpg"
+  }
+]);
