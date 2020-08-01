f76be3d7dcb72107d58206d7390493ab40cb4730
Евгений Арапов <arapov.evgeniy@gmail.com>
2020-08-01 17:23:51 +0300

    refactor: add babelrc, add logger

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,10 +1,4 @@
 export default store => next => action => {
-  console.log("============================================")
-  console.log(action.type)
-  console.log("action.name: ", action.name)
-  console.log(action.payload)
-  console.log("============================================")
-
   try {
     next(action)
   } catch (error) {
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import errorHandler from "middleware/errorHandler"
+import logger from "middleware/logger"
 import rootReducer from "./reducers"
 
 export default createStore(
     rootReducer,
     applyMiddleware(
-      thunk
-      ,errorHandler
+      thunk,
+      errorHandler,
+      logger
     )
 )
